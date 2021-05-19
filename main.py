8bfad782bd33fbe20f71a0dd10ddd2bd40ee477a
betopompolo <adalberto.pompolo@taqtile.com.br>
2021-05-18 22:51:21 -0300

    Remove TODO

--- main.py
+++ main.py
@@ -9,6 +9,5 @@ if __name__ == '__main__':
     for business in load_business(categories=business_cats):
         business_ids.append(business['business_id'])
 
-    # TODO: The review file is has more than 4gb. islice is required here
     for review in itertools.islice(load_reviews(business_ids=business_ids), 5000):
         pass
