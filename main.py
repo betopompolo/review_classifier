dfb9e712da7872a8b2f62743a9569273691fbaee
betopompolo <adalberto.pompolo@taqtile.com.br>
2021-05-19 22:32:58 -0300

    lsi analysis

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import itertools
+from typing import List
+
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from dataset_parser import load_business, load_reviews
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt
+
+
+def lsi_analysis(reviews_texts: List[str]) -> TruncatedSVD:
+    word_bag = TfidfVectorizer(lowercase=True).fit_transform(reviews_texts)
+    lsi = TruncatedSVD(n_components=100, n_iter=100)
+    return lsi.fit_transform(word_bag)
+
 
 if __name__ == '__main__':
     business_cats = ['restaurants', 'hotels']
-    business_ids = []
+    business_ids = [b.business_id for b in load_business(categories=business_cats)]
 
-    for business in load_business(categories=business_cats):
-        business_ids.append(business.business_id)
-
-    for review in itertools.islice(load_reviews(business_ids=business_ids), 5000):
-        print(f'business: {review.business_id} -> star: {review.stars}')
+    texts = [r.text for r in itertools.islice(load_reviews(business_ids=business_ids), 10)]
+    lsi_matrix = lsi_analysis(texts)
+    plt.imshow(lsi_matrix)
+    plt.colorbar()
+    plt.show()
